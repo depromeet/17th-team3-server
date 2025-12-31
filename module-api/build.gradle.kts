7d81631bb3338a2dbe328ae6ae618471f0eee48b
parkmineum <a01087164428@gmail.com>
2025-12-31 21:20:38 +0900

    [REFACTOR] #136 : api 트레이스 모니터링 기능 추가

--- module-api/build.gradle.kts
+++ module-api/build.gradle.kts
@@ -21,6 +21,11 @@ dependencies {
     // Prometheus 매트릭
     implementation("io.micrometer:micrometer-registry-prometheus")
 
+    // Tracing (Tempo / OpenTelemetry)
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+    implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.0.1")
+
     // Jackson Kotlin 모듈
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
