15f97da846221bec4e54909106a2f9f32e78e22b
parkmineum <a01087164428@gmail.com>
2026-01-17 23:05:18 +0900

    [REFACTOR] : 개발 환경 WAS 간 스웨거 통합 및 네트워크 설정 추가

--- ssolv-api-common/src/main/kotlin/org/depromeet/team3/config/WebMvcConfig.kt
+++ ssolv-api-common/src/main/kotlin/org/depromeet/team3/config/WebMvcConfig.kt
@@ -3,6 +3,7 @@ package org.depromeet.team3.config
 import org.depromeet.team3.common.resolver.UserIdArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 /**
@@ -18,5 +19,22 @@ class WebMvcConfig(
      */
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(userIdArgumentResolver)
+    }
+
+    /**
+     * CORS 설정
+     * 로컬 환경에서 Core API (8080)에서 Place API (8081)의 Swagger 문서를 가져올 수 있도록 허용
+     */
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:8080",
+                "http://localhost:8081",
+                "https://api.ssolv.site",
+                "https://ssolv.site"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
